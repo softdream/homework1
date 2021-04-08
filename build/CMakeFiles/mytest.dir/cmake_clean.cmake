c1fcc94445c20d7a1db6fda8f8b70a0a6b2a7503
daofeng <1336077399@qq.com>
2021-04-08 23:28:14 +0800

    homework

---
+++ build/CMakeFiles/mytest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mytest.dir/src/main.cpp.o"
+  "bin/mytest.pdb"
+  "bin/mytest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mytest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
